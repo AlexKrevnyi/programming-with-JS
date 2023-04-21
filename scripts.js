86d94b07ddeb09dd29f81d4701d60022c46b3a54
alex <sania9991@yahoo.com>
2023-04-21 16:24:26 -0700

    Week 1. Repetitive tasks with loops

--- scripts.js
+++ scripts.js
@@ -66,6 +66,52 @@ switch(day) {
 
 
 
+///////Exersice: Repetitive tasks with loops
+
+//Task 1
+
+for (var i = 1; i <= 5; i++) {
+	console.log(i);	
+}
+console.log('Counting completed');
+
+//Task 2
+
+for (var i = 5; i >= 1; i--) {
+	console.log(i);
+}
+
+console.log('Countdown completed');
+
+//Task 3
+
+var i = 1;
+while (i <= 5) {
+	console.log(i);
+	i++;
+}
+
+console.log('Counting completed');
+
+
+//Task 4
+var i = 5;
+while (i > 0) {
+	console.log(i);
+	i = i -1;
+}
+
+console.log('Countdown completed');
+
+
+//Task 5
+
+var i = 2018;
+while (i <= 2022) {
+	console.log(i);
+	i++;
+}
+console.log('Counting completed');
 
 
 
