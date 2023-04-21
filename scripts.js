ada7fae6fb284080dfd6bd59e59873a6e7616868
alex <sania9991@yahoo.com>
2023-04-21 12:04:09 -0700

    Acquaintance with JS

---
+++ scripts.js
@@ -0,0 +1,16 @@
+console.log(2 + 2);
+console.log("Hello, World");
+console.log(1 + 2 + 3 + 5 + 10);
+console.log(35 - 20);
+console.log(8/2)
+console.log(3 > 2);
+console.log(2 > 3);
+console.log(10 == 10);
+console.log(10 == 9);
+
+var score = "8";
+console.log("mid-level skills:", 0 < score && score < 10);
+var timeRemaining = "0";
+
+var energy = "10";
+console.log("Game over:", timeRemaining == 0 || energy == 0 );
