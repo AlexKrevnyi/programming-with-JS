ff007636cb753493b455ca515e3140f6421d9d2d
alex <sania9991@yahoo.com>
2023-04-21 13:21:54 -0700

    Week 1. Conditionals and Loops.Practice conditional statements

--- scripts.js
+++ scripts.js
@@ -14,3 +14,59 @@ var timeRemaining = "0";
 
 var energy = "10";
 console.log("Game over:", timeRemaining == 0 || energy == 0 );
+
+
+//04.21.2023
+
+var age = 25;
+if (age >= 65) {
+	console.log('You get your income from your pension');
+}
+else if (65 > age && age >= 18) {
+	console.log('Each month you get salary');
+}
+
+else if (age < 18) {
+	console.log('You get an alowance');
+}
+else {
+	console.log('The value of the age variable is not numerical');
+
+}
+
+///Second exercise
+
+var day = 'Friday';
+
+switch(day) {
+	case 'Monday':
+		console.log("Walking a dog");
+		break;
+	case 'Tuesday':
+		console.log("Go to the beach");
+		break;
+	case 'Wednesday':
+		console.log("Wash my car");
+		break;
+	case 'Thursday':
+		console.log("Drink some coffee");
+		break;
+	case 'Friday':
+		console.log("Go to the shopping");
+		break;	
+	case 'Saturday':
+		console.log("Clean my house");
+		break;
+	case 'Sunday':
+		console.log("Buy a new book");
+		break;
+	default: 
+	console.log("There is no such day");						
+}	
+
+
+
+
+
+
+
