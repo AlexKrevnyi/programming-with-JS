05ff10790238bc8b79e46fe33e2c3c0ac012cbed
alex <sania9991@yahoo.com>
2023-05-11 16:29:19 -0700

    Week 2. A closer look at strings

--- scripts.js
+++ scripts.js
@@ -285,3 +285,37 @@ var letters = ['abc'];
 for (var i = 0; i < letters.length; i++) {
 	console.log(letters[i]);
 }
+
+
+var veggies = ['onion', 'parsley', 'carrot'];
+
+console.log(veggies.length);
+
+console.log(veggies[0]);
+console.log(veggies[1]);
+
+for (var i = 0; i < veggies.length; i++) {
+	console.log(veggies[i]);
+}
+
+///New exercise
+
+var greeting = 'Howdy';
+
+console.log(greeting.length);
+
+console.log(greeting[0]);
+console.log(greeting[2]);
+
+for (var i = 0; i < greeting.length; i++) {
+	console.log(greeting[i]);
+}
+
+///New exercise
+
+var greet = 'Hello ';
+var user = 'Lisa';
+
+console.log(greet + user);
+console.log(greet.concat(user));
+
