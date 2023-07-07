40726a40810518b00523b2d7599684ff12feeccf
alex <sania9991@yahoo.com>
2023-07-06 19:41:09 -0700

    07/06-1

--- app.js
+++ app.js
@@ -28,3 +28,44 @@ console.log(firstListItem.innerHTML);
 li.innerText = 'X-men';
 
 li.style.fontSize = '2rem';
+
+//Modyfing Attributes and Classes
+
+li.setAttribute('id', 'list-items');
+li.setAttribute('class', 'list-items');
+li.removeAttribute('id');
+
+const baza = document.querySelector('#main-heading');
+
+console.log(baza.getAttribute('id'));
+
+li.classList.add('baza');
+
+console.log(li.classList.contains('baza'));
+
+//Remove Elements
+
+li.remove();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
