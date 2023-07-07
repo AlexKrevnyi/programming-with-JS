c5d804a74d815a2770408dcb06cc52b32d4e4f26
alex <sania9991@yahoo.com>
2023-07-07 13:56:48 -0700

    Child/Sibling/Parent Node traverssal

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //FreeCodeCamp
-
+/*
 const title = document.querySelector('#main-heading');
 
 title.style.color = 'red';
@@ -46,6 +46,57 @@ console.log(li.classList.contains('baza'));
 //Remove Elements
 
 li.remove();
+
+
+// Parent Node Traversal
+
+let ul = document.querySelector('ul');
+
+console.log(ul.parentNode.parentNode);
+console.log(ul.parentElement.parentElement);
+
+const html = document.documentElement;
+
+console.log(html.parentNode);
+console.log(html.parentElement);
+
+
+
+// Child Node Traversal
+
+let ul = document.querySelector('ul');
+
+console.log(ul.childNodes);
+console.log(ul.firstChild);
+console.log(ul.lastChild);
+
+ul.childNodes[1].style.backgroundColor = 'red';
+
+
+
+let ul = document.querySelector('ul');
+
+console.log(ul.children);
+console.log(ul.firstElementChild);
+console.log(ul.lastElementChild);
+
+
+
+// Sibling Node Traversal
+
+let ul = document.querySelector('ul');
+const div = document.querySelector('div');
+
+console.log(div.childNodes);
+
+console.log(ul.previousElementSibling);
+console.log(ul.nextElementSibling);
+
+*/
+
+
+
+
 
 
 
